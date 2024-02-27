fbac94eea5df1ac838338635ac96ac9834ab137c
JuanFeSR <juanfesr01@gmail.com>
2024-02-27 12:25:33 -0500

    Componente de Card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,13 @@
 import Layout from "../../Components/Layout/index";
+import Card from "../../Components/Card/index";
 
 function Home() {
-  return <Layout className="bg-slate-500">Home</Layout>;
+  return (
+    <Layout className="bg-slate-500">
+      Home
+      <Card />
+    </Layout>
+  );
 }
 
 export default Home;
