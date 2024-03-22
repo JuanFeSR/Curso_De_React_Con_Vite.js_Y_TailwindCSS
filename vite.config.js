c57cb2952e13979a429cb22873fde940dc920a5d
JuanFeSR <juanfesr01@gmail.com>
2024-03-22 15:45:14 -0500

    Corrección Deploy GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Curso_De_React_Con_Vite.js_Y_TailwindCSS",
+  base: "/Curso_De_React_Con_Vite.js_Y_TailwindCSS/",
 });
